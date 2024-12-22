b8b021b71b86a28a023a930b35f4c8f9cd596a0a
Muhammad Usman <142773789+usmanjutt47@users.noreply.github.com>
2024-12-22 20:24:52 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import LineChartComponent from "./components/LineChart";
+import BarChart from "./components/BarChart";
+import PieChartComponent from "./components/PieChart";
+import ProgressChartComponent from "./components/ProgressChart";
+import ContributionGraphComponent from "./components/ContributionGraph";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 10, paddingTop: 30 }}>
+        <StatusBar style="dark" />
+        <LineChartComponent />
+        <PieChartComponent />
+        <BarChart />
+        <ProgressChartComponent />
+        <ContributionGraphComponent />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
